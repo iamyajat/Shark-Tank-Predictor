e73ff07bac7d277909b838fc23866872af03d883
Vignesh Duraisamy <vigneshd332@gmail.com>
2023-06-30 23:05:43 +0530

    feat: add mainpage UI

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI
+import os
+from os.path import join, dirname
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from dotenv import load_dotenv
 import model
 
 
-app = FastAPI(title="Shark Tank India Predictor API", version="0.1.0", description="API for Shark Tank India Predictor")
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+PORT = int(os.getenv("PORT", 8000))
+DEV = bool(os.getenv("DEV", False))
+WORKERS = int(os.getenv("WORKERS", 1))
+
+app = FastAPI(title="Shark Tank India Predictor API", version="0.1.0",
+              description="API for Shark Tank India Predictor")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Up and running!"}
+async def root(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
 
-@app.get("/predict")
-async def predict(ask_amount: int, ask_equity:int, ask_valuation: int, probabilities: bool = False):
+
+@app.post("/predict")
+async def predict(ask_amount: int, ask_equity: int, ask_valuation: int, probabilities: bool = False):
     data = [[ask_amount, ask_equity, ask_valuation]]
     if probabilities:
         p = model.predict_proba(data)
@@ -23,3 +42,6 @@ async def predict(ask_amount: int, ask_equity:int, ask_valuation: int, probabili
         "deal": bool(p[0])
     }
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT,
+                reload=DEV, workers=WORKERS)
